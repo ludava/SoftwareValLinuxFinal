d31432b78b129fc72177b43c4712b1b44cc62e05
Luis Damian Valdes <38168334+ludava@users.noreply.github.com>
2020-12-02 13:24:07 -0600

    Created TestCase2

---
+++ src/test/java/org/apache/commons/mail/TestCase2.java
@@ -0,0 +1,22 @@
+package org.apache.commons.mail;
+
+import org.apache.commons.mail.Email;
+import org.apache.commons.mail.EmailException;
+import org.apache.commons.mail.SimpleEmail;
+
+import junit.framework.TestCase;
+
+public class TestCase2 extends TestCase{
+	Email testEmail;
+	public void setUp() {
+		testEmail = new SimpleEmail();
+	}
+	public void testAddCcMulti() throws EmailException {
+		String [] testEmails = new String[] {"a@b.com", "b@c.com"};
+		testEmail.addCc(testEmails);
+		
+		assertEquals("a@b.com", testEmail.getCcAddresses().get(0).toString());
+		assertEquals("b@c.com", testEmail.getCcAddresses().get(1).toString());
+	}
+
+}
